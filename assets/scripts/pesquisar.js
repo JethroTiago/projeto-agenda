2ff3c4b5f48c7c3d7a4cbd07521413ddb9a9a0b2
Jethro <jethrotiago@hotmail.com>
2023-09-13 23:14:34 -0300

    Desenvolvendo lógica para o campo de pesquisa

--- assets/scripts/pesquisar.js
+++ assets/scripts/pesquisar.js
@@ -2,10 +2,17 @@ const analisar = document.querySelector("#btn_pesquisa");
 const textoDePesquisa = document.querySelector("#txt_pesquisa");
 
 analisar.addEventListener("click", (evt) => {
-    if (textoDePesquisa.value == "") {
+    const valorPesquisa = textoDePesquisa.value
+    if (valorPesquisa == "") {
         alert("Digite algo no campo para pesquisar.");
         textoDePesquisa.focus();
         return;
     }
-    console.log(document.getElementsByName("pesquisa").value)
+    const pesquisa = document.querySelector("input[name=pesquisa]:checked").value;
+    let entpoint = `http://127.0.0.1:1880/pesquisarcontatos/${pesquisa}/${valorPesquisa}`;
+    fetch(entpoint)
+    .then(res => res.json())
+    .then(res => {
+        console.log(res);
+    })
 });
