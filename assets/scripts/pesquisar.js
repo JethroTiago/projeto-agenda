a0923854bc31741ad1c92be63e24b01180fb6f7c
Jethro <jethrotiago@hotmail.com>
2023-09-18 13:21:44 -0300

    Finalizado requisição de pesquisa

--- assets/scripts/pesquisar.js
+++ assets/scripts/pesquisar.js
@@ -1,18 +1,45 @@
-const analisar = document.querySelector("#btn_pesquisa");
-const textoDePesquisa = document.querySelector("#txt_pesquisa");
+const btn_pesquisa = document.querySelector("#btn_pesquisa");
+const txt_pesquisa = document.querySelector("#txt_pesquisa");
+const dados = document.querySelector("#dados");
 
-analisar.addEventListener("click", (evt) => {
-    const valorPesquisa = textoDePesquisa.value
+btn_pesquisa.addEventListener("click", (evt) => {
+    const valorPesquisa = txt_pesquisa.value
     if (valorPesquisa == "") {
         alert("Digite algo no campo para pesquisar.");
-        textoDePesquisa.focus();
+        txt_pesquisa.focus();
         return;
     }
     const pesquisa = document.querySelector("input[name=pesquisa]:checked").value;
-    let entpoint = `http://127.0.0.1:1880/pesquisarcontatos/${pesquisa}/${valorPesquisa}`;
+    const entpoint = `http://127.0.0.1:1880/pesquisarcontatos/${pesquisa}/${valorPesquisa}`;
     fetch(entpoint)
     .then(res => res.json())
     .then(res => {
-        console.log(res);
+        dados.innerHTML = "";
+        res.forEach((elemento) => {
+            const linha = document.createElement("div");
+            linha.setAttribute("class", "dados_linhas");
+
+            const coluna1 = document.createElement("div");
+            coluna1.setAttribute("class", "col c1");
+            coluna1.innerHTML = elemento.n_contato_contato;
+            linha.appendChild(coluna1);
+
+            const coluna2 = document.createElement("div");
+            coluna2.setAttribute("class", "col c2");
+            coluna2.innerHTML = elemento.s_nome_contato;
+            linha.appendChild(coluna2);
+
+            const coluna3 = document.createElement("div");
+            coluna3.setAttribute("class", "col c3");
+            coluna3.innerHTML = elemento.s_telefone_contato;
+            linha.appendChild(coluna3);
+
+            const coluna4 = document.createElement("div");
+            coluna4.setAttribute("class", "col c4");
+            coluna4.innerHTML = elemento.s_email_contato;
+            linha.appendChild(coluna4);
+
+            dados.appendChild(linha);
+        })
     })
 });
